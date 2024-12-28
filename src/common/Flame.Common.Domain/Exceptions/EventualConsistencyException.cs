6b76bf4939d3ca5df610243b20216b25e7a93378
mreshboboyev <mreshboboyev@gmail.com>
2024-12-28 15:35:57 +0500

    Common.Domain exceptions : create Exceptions(EventualConsistencyException and ValidationException);

---
+++ src/common/Flame.Common.Domain/Exceptions/EventualConsistencyException.cs
@@ -0,0 +1,43 @@
+﻿namespace Flame.Common.Domain.Exceptions;
+
+/// <summary>
+/// Represents an exception for eventual consistency errors.
+/// </summary>
+public class EventualConsistencyException : Exception
+{
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EventualConsistencyException"/> class with the specified error code and error message.
+    /// </summary>
+    /// <param name="errorCode">The error code.</param>
+    /// <param name="errorMessage">The error message.</param>
+    /// <param name="details">Additional details about the error.</param>
+    public EventualConsistencyException(
+        string errorCode,
+        string errorMessage,
+        List<string>? details = null)
+        : base(errorMessage)
+    {
+        ErrorCode = errorCode;
+        ErrorMessage = errorMessage;
+        Details = details ?? [];
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Gets the error code.
+    /// </summary>
+    public string ErrorCode { get; }
+
+    /// <summary>
+    /// Gets the error message.
+    /// </summary>
+    public string ErrorMessage { get; }
+
+    /// <summary>
+    /// Gets the list of additional details about the error.
+    /// </summary>
+    public List<string> Details { get; }
+}
---
+++ src/common/Flame.Common.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,48 @@
+﻿namespace Flame.Common.Domain.Exceptions;
+
+/// <summary>
+/// Represents an exception for validation errors.
+/// </summary>
+[Serializable]
+public class ValidationException : Exception
+{
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidationException"/> class with the specified validation errors.
+    /// </summary>
+    /// <param name="errors">The validation errors.</param>
+    public ValidationException(IEnumerable<string> errors)
+        : base("Validation failed.")
+    {
+        Errors = errors?.ToList() ?? [];
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidationException"/> class with the specified error message.
+    /// </summary>
+    /// <param name="error">The error message.</param>
+    public ValidationException(string error)
+        : base(error)
+    {
+        Errors = [];
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidationException"/> class with the specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="inner">The exception that is the cause of the current exception.</param>
+    public ValidationException(string message, Exception inner)
+        : base(message, inner)
+    {
+        Errors = [ inner.Message ];
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Gets the list of validation errors.
+    /// </summary>
+    public IReadOnlyList<string> Errors { get; set; }
+}
