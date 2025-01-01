fc0be41ec85c766859be520c2b86f909471fec2e
mreshboboyev <mreshboboyev@gmail.com>
2025-01-01 14:57:34 +0500

    BasketContext.Infrastructure extensions : create UnitOfWork implementation and layer extensions

---
+++ src/Flame.BasketContext.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Flame.BasketContext.Application.Repositories;
+
+namespace Flame.BasketContext.Infrastructure.Persistence;
+
+public class UnitOfWork(BasketAppDbContext dbContext) : IUnitOfWork
+{
+    private readonly BasketAppDbContext _dbContext = 
+        dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // Save changes to the database
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public void Dispose() => _dbContext.Dispose();
+}
