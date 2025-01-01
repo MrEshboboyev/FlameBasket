34fa8cac0fc95a454819a99f21cfc027bef51e48
mreshboboyev <mreshboboyev@gmail.com>
2025-01-01 13:21:24 +0500

    BasketContext.Infrastructure entities : create entities (basket, basketItem, coupon, customer, seller)

---
+++ src/Flame.BasketContext.Infrastructure/Entities/CustomerEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Flame.BasketContext.Infrastructure.Entities;
+
+public class CustomerEntity
+{
+    public Guid Id { get; set; }
+    public bool IsEliteMember { get; set; }
+    public ICollection<BasketEntity> Baskets { get; set; }
+}
---
+++ src/Flame.BasketContext.Infrastructure/Entities/SellerEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Flame.BasketContext.Infrastructure.Entities;
+
+public class SellerEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public float Rating { get; set; }
+    public decimal ShippingLimit { get; set; }
+    public decimal ShippingCost { get; set; }
+}
