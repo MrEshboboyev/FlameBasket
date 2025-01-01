e5ef1375be35226ee75625f7c7d258c53fb8447f
mreshboboyev <mreshboboyev@gmail.com>
2025-01-01 15:14:46 +0500

    BasketContext.Api controllers : create BaseController (partial abstract class)

---
+++ src/Flame.BasketContext.Api/Controllers/BaseController.cs
@@ -0,0 +1,95 @@
+﻿using Flame.BasketContext.Api.Extensions;
+using Flame.Common.Domain.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flame.BasketContext.Api.Controllers;
+
+[ApiController]
+public abstract partial class BaseController : ControllerBase
+{
+    protected readonly ILogger Logger;
+
+    private readonly Dictionary<ErrorType,
+        Func<string?, IEnumerable<string>?, ObjectResult>> _errorHandlers;
+
+    protected BaseController(ILogger logger)
+    {
+        Logger = logger;
+        _errorHandlers = new Dictionary<ErrorType,
+            Func<string?, IEnumerable<string>?, ObjectResult>>
+        {
+            {
+                ErrorType.Conflict,
+                ConflictResponse
+            },
+            {
+                ErrorType.NotFound,
+                NotFoundResponse
+            },
+            {
+                ErrorType.BadRequest,
+                BadRequestResponse
+            },
+            {
+                ErrorType.Validation,
+                ValidationResponse
+            },
+            {
+                ErrorType.Unexpected,
+                UnexpectedResponse
+            }
+        };
+    }
+
+    protected ObjectResult HandleError(IDomainError error)
+    {
+        if (_errorHandlers.TryGetValue(error.ErrorType, out var handler))
+        {
+            return handler(
+                error.ErrorMessage,
+                error.Errors);
+        }
+
+        throw new InvalidOperationException($"Unsupported error type: {error.ErrorType}");
+    }
+
+    protected ObjectResult NotFoundResponse(
+        string? message = null,
+        IEnumerable<string>? errors = null) =>
+        NotFound(ProblemDetailsFactory.CreateNotFound(
+            HttpContext,
+            message,
+            errors));
+
+    protected ObjectResult BadRequestResponse(
+        string? details = null,
+        IEnumerable<string>? errors = null) =>
+        BadRequest(ProblemDetailsFactory.CreateBadRequest(
+            HttpContext,
+            details,
+            errors));
+
+    protected ObjectResult ConflictResponse(
+        string? details = null,
+        IEnumerable<string>? errors = null) =>
+        Conflict(ProblemDetailsFactory.CreateConflict(
+            HttpContext,
+            details,
+            errors));
+
+    protected ObjectResult ValidationResponse(
+        string? details = null,
+        IEnumerable<string>? errors = null) =>
+        BadRequest(ProblemDetailsFactory.CreateValidation(
+            HttpContext,
+            details,
+            errors));
+
+    protected ObjectResult UnexpectedResponse(
+        string? details = null,
+        IEnumerable<string>? errors = null) =>
+        BadRequest(ProblemDetailsFactory.CreateUnexpectedResponse(
+            HttpContext,
+            details,
+            errors));
+}
