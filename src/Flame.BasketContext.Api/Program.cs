54fc392a375af80ae6dbe2b1cd8892c1cfad0ccf
mreshboboyev <mreshboboyev@gmail.com>
2025-01-01 15:22:33 +0500

    BasketContext.Api run : configure Program.cs with Layers

--- src/Flame.BasketContext.Api/Program.cs
+++ src/Flame.BasketContext.Api/Program.cs
@@ -1,14 +1,30 @@
+using System.Reflection;
+using Flame.BasketContext.Application;
+using Flame.BasketContext.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+#region Add Layers
+
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+#endregion
+
+// Register MediatR
+builder.Services.AddMediatR(cfg => 
+    cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Apply migrations at runtime
+app.ApplyMigrations();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
