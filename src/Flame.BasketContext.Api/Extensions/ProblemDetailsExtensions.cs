1ee50532bb328cb62fcb263d540044dbf74a0d68
mreshboboyev <mreshboboyev@gmail.com>
2025-01-01 15:10:32 +0500

    Create BasketContext.Api : create Extensions (problem Details extensions)

---
+++ src/Flame.BasketContext.Api/Extensions/ProblemDetailsExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Flame.BasketContext.Api.Extensions;
+
+public static class ProblemDetailsExtensions
+    {
+        public static ProblemDetails CreateNotFound(
+            this ProblemDetailsFactory detailsFactory,
+            HttpContext context,
+            string? details = null,
+            IEnumerable<string>? errors = null)
+        {
+            return CreateProblemDetailsWith(
+                detailsFactory,
+                StatusCodes.Status404NotFound,
+                context, 
+                details,
+                errors);
+        }
+
+        public static ProblemDetails CreateBadRequest(
+            this ProblemDetailsFactory detailsFactory,
+            HttpContext context,
+            string? details = null,
+            IEnumerable<string>? errors = null)
+        {
+            return CreateProblemDetailsWith(
+                detailsFactory,
+                StatusCodes.Status400BadRequest,
+                context,
+                details,
+                errors);
+        }
+
+        public static ProblemDetails CreateConflict(
+            this ProblemDetailsFactory detailsFactory,
+            HttpContext context,
+            string? details = null,
+            IEnumerable<string>? errors = null)
+        {
+            return CreateProblemDetailsWith(
+                detailsFactory,
+                StatusCodes.Status409Conflict,
+                context, 
+                details,
+                errors);
+        }
+
+        public static ProblemDetails CreateValidation(
+           this ProblemDetailsFactory detailsFactory,
+           HttpContext context,
+           string? details = null,
+            IEnumerable<string>? errors = null)
+        {
+            return CreateProblemDetailsWith(
+                detailsFactory,
+                StatusCodes.Status400BadRequest,
+                context,
+                details,
+                errors);
+        }
+
+        public static ProblemDetails CreateUnexpectedResponse(
+           this ProblemDetailsFactory detailsFactory,
+           HttpContext context,
+           string? details = null,
+           IEnumerable<string>? errors = null)
+        {
+            return CreateProblemDetailsWith(
+                detailsFactory,
+                StatusCodes.Status500InternalServerError,
+                context,
+                details,
+                errors);
+        }
+
+
+        private static ProblemDetails CreateProblemDetailsWith(
+           ProblemDetailsFactory detailsFactory, 
+           int statusCode,
+           HttpContext context,
+           string? message = null,
+           IEnumerable<string>? errors = null)
+        {
+            if (errors != null && errors.Any())
+            {
+                var errorList = new StringBuilder();
+                errorList.AppendJoin(",", errors);
+
+                return detailsFactory.CreateProblemDetails(
+                    context,
+                    statusCode: statusCode,
+                    detail: errorList.ToString());
+            }
+            else
+                return detailsFactory.CreateProblemDetails(
+                    context,
+                    statusCode: statusCode, 
+                    detail: message);
+        }
+    }
