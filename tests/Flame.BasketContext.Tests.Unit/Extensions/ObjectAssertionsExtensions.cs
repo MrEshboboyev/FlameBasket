d3fb0f2bac707946962894962c399b7b9d2187ec
mreshboboyev <mreshboboyev@gmail.com>
2025-01-04 11:28:34 +0500

    tests : BasketContext.Tests.Unit extensions for simplify : create ObjectAssertionsExtensions.cs, ensuring domain event equivalence checks are accurate and concise.

---
+++ tests/Flame.BasketContext.Tests.Unit/Extensions/ObjectAssertionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Flame.Common.Domain.Events;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace Flame.BasketContext.Tests.Unit.Extensions;
+
+public static class ObjectAssertionsExtensions
+{
+    public static AndConstraint<ObjectAssertions> BeEquivalentEventTo<TExpectation>(
+        this ObjectAssertions obj,
+        TExpectation expectation, 
+        string because = "",
+        params object[] becauseArgs)
+        where TExpectation : IDomainEvent
+    {
+        return obj.BeEquivalentTo(expectation,
+            options => options
+                .Excluding(t => t.OccurredOnUtc)
+                .Excluding(t => t.Id));
+    }
+}
